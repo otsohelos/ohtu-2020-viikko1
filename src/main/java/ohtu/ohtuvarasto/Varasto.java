42d92f11fab86f4e63352bf79b1417f3dd7db377
Otso Helos <17408120+otsohelos@users.noreply.github.com>
2020-11-06 14:30:47 +0200

    this should fix the code

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -2,73 +2,72 @@ package ohtu.ohtuvarasto;
 
 public class Varasto {
 
-    // --- piilotettu tietorakenteen toteutus: ---
-    private double tilavuus;  // paljonko varastoon mahtuu,  > 0
-    private double saldo;     // paljonko varastossa on nyt, >= 0
+  // --- piilotettu tietorakenteen toteutus: ---
+  private double tilavuus; // paljonko varastoon mahtuu, > 0
+  private double saldo; // paljonko varastossa on nyt, >= 0
 
-    // --- konstruktorit: ---
-    public Varasto(double tilavuus) {  // tilavuus on annettava
-        if (tilavuus > 0.0) {
-            this.tilavuus = tilavuus;
-        } else { // virheellinen, nollataan
-            this.tilavuus = 0.0;  // => käyttökelvoton varasto
-        }
-        saldo = 0;     // oletus: varasto on tyhjä
+  // --- konstruktorit: ---
+  public Varasto(double tilavuus) { // tilavuus on annettava
+    if (tilavuus > 0.0) {
+      this.tilavuus = tilavuus;
+    } else { // virheellinen, nollataan
+      this.tilavuus = 0.0; // => käyttökelvoton varasto
     }
+    saldo = 0; // oletus: varasto on tyhjä
+  }
 
-    public Varasto(double tilavuus, double alkuSaldo)
-     { // kuormitetaan
-        if (tilavuus > 0.0) { // jos varasto on käyttökelpoinen
-            this.tilavuus = tilavuus;
-             this.saldo = Math.max(0.0, alkuSaldo); // jos alkuSaldo alle nolla, saldo nolla
-            this.saldo = Math.min(tilavuus, this.saldo); // jos saldo yli tilavuuden, niin saldo on koko tilavuus
-        } else { // jos varaston koko 0 tai negatiivinen
-            this.tilavuus = 0.0;
-            this.saldo = 0.0;
-        }
+  public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
+    if (tilavuus > 0.0) { // jos varasto on käyttökelpoinen
+      this.tilavuus = tilavuus;
+      this.saldo = Math.max(0.0, alkuSaldo); // jos alkuSaldo alle nolla, saldo nolla
+      this.saldo = Math.min(tilavuus, this.saldo); // jos saldo yli tilavuuden, niin saldo on koko tilavuus
+    } else { // jos varaston koko 0 tai negatiivinen
+      this.tilavuus = 0.0;
+      this.saldo = 0.0;
     }
+  }
 
-// --- ottavat aksessorit eli getterit: ---
-    public double getSaldo() {
-        return saldo;
-    }
+  // --- ottavat aksessorit eli getterit: ---
+  public double getSaldo() {
+    return saldo;
+  }
 
-    public double getTilavuus() {
-        return tilavuus;
-    }
+  public double getTilavuus() {
+    return tilavuus;
+  }
 
-    public double paljonkoMahtuu() {  // huom: ominaisuus voidaan myös laskea
-        return tilavuus - saldo;        //  ei tarvita erillistä kenttää vielaTilaa tms.
-    }
+  public double paljonkoMahtuu() { // huom: ominaisuus voidaan myös laskea
+    return tilavuus - saldo; // ei tarvita erillistä kenttää vielaTilaa tms.
+  }
 
-    // --- asettavat aksessorit eli setterit: ---
-    public void lisaaVarastoon(double maara) {
-        if (maara < 0) { // virhetilanteessa voidaan tehdä
-            return;       // tällainen pikapoistuminenkin!
-        }
-        if (maara <= paljonkoMahtuu()) { // omia aksessoreita voi kutsua
-            saldo = saldo + maara;          // ihan suoraan sellaisinaan
-        } else {
-            saldo = tilavuus;  // täyteen ja ylimäärä hukkaan!
-        }
+  // --- asettavat aksessorit eli setterit: ---
+  public void lisaaVarastoon(double maara) {
+    if (maara < 0) { // virhetilanteessa voidaan tehdä
+      return; // tällainen pikapoistuminenkin!
     }
+    if (maara <= paljonkoMahtuu()) { // omia aksessoreita voi kutsua
+      saldo = saldo + maara; // ihan suoraan sellaisinaan
+    } else {
+      saldo = tilavuus; // täyteen ja ylimäärä hukkaan!
+    }
+  }
 
-    public double otaVarastosta(double maara) {
-        if (maara < 0) { // virhetilanteessa voidaan tehdä 
-            return 0.0;   // tällainen pikapoistuminenkin!
-        }
-        if (maara > saldo) {          // annetaan mitä voidaan
-            double kaikkiMitaVoidaan = saldo;
-            saldo = 0.0;               // ja tyhjäksi menee
-            return kaikkiMitaVoidaan;  // poistutaan saman tien
-        }
-        // jos tänne päästään, kaikki pyydetty voidaan antaa
-        saldo = saldo - maara;  // vähennetään annettava saldosta
-        return maara;
+  public double otaVarastosta(double maara) {
+    if (maara < 0) { // virhetilanteessa voidaan tehdä
+      return 0.0; // tällainen pikapoistuminenkin!
     }
+    if (maara > saldo) { // annetaan mitä voidaan
+      double kaikkiMitaVoidaan = saldo;
+      saldo = 0.0; // ja tyhjäksi menee
+      return kaikkiMitaVoidaan; // poistutaan saman tien
+    }
+    // jos tänne päästään, kaikki pyydetty voidaan antaa
+    saldo = saldo - maara; // vähennetään annettava saldosta
+    return maara;
+  }
 
-    // --- Merkkijonoesitys Varasto-oliolle: ----
-    public String toString() {
-        return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
-    }
+  // --- Merkkijonoesitys Varasto-oliolle: ----
+  public String toString() {
+    return ("saldo = " + saldo + ", vielä tilaa " + paljonkoMahtuu());
+  }
 }
